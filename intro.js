a2176198eb944830ad9634b89dad3f3a7aadc2c9
Azeez Yusuf O. <highshow74@gmail.com>
2025-12-06 06:51:46 +0000

    Initial commit: User's introduction

---
+++ intro.js
@@ -0,0 +1,9 @@
+const user = {
+    name: "Yusuf Abdulazeez",
+    age: 25,
+    favoriteProgrammingLanguage: "Javascript"
+};
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.favoriteProgrammingLanguage);
